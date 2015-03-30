b58acf81ba79363608204b11f6d6e93ab15ce650
Konstantin Kostelano <kostelano-88@mail.ru>
2015-03-30 00:07:37 +0000

    default.php / fix button "Clear"
    more info: https://z-index.net/en/forum/support/xmap/small-bug-button-clear-does-not-work.html

--- admin/views/sitemaps/tmpl/default.php
+++ admin/views/sitemaps/tmpl/default.php
@@ -63,7 +63,7 @@ JFactory::getDocument()->addStyleDeclaration('#toolbar-power-cord{float:right;}@
                             class="icon-search"></i></button>
                     <button type="button" class="btn hasTooltip"
                             title="<?php echo JHtml::tooltipText('JSEARCH_FILTER_CLEAR'); ?>"
-                            onclick="document.id('filter_search').value='';this.form.submit();"><i
+                            onclick="document.getElementById('filter_search').value='';this.form.submit();"><i
                             class="icon-remove"></i></button>
                 </div>
                 <div class="btn-group pull-right hidden-phone">
